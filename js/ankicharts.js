922332748314955e9458ca20aaa9122d0e4a2624
yolandaw <yw.yunwing@gmail.com>
2013-11-11 23:33:41 -0800

    made the graphs work

--- js/ankicharts.js
+++ js/ankicharts.js
@@ -1,7 +1,7 @@
-		// $(document).ready(function(){$("#nextBtn").click(&("#canvas.text("canvas2"))});
+		$(document).ready(function(){
 
 		var barChartData = {
-			labels : ["January","February","March","April","May","June","July"],
+			labels : ["Jan","Feb","Mar","Apr","May","June","July"],
 			datasets : [
 				{
 					fillColor : "rgba(220,220,220,0.5)",
@@ -11,7 +11,7 @@
 				{
 					fillColor : "rgba(151,187,205,0.5)",
 					strokeColor : "rgba(151,187,205,1)",
-					data : [28,48,40,19,96,27,100]
+					data : [28,48,40,19,96,27,90]
 				}
 			]
 			
@@ -20,30 +20,37 @@
 	var myLine = new Chart(document.getElementById("canvas").getContext("2d")).Bar(barChartData);
 	
 
-	var chartData = [
-			{
-				value : Math.random(),
-				color: "#D97041"
-			},
-			{
-				value : Math.random(),
-				color: "#C7604C"
-			},
-			{
-				value : Math.random(),
-				color: "#21323D"
-			},
-			{
-				value : Math.random(),
-				color: "#9D9B7F"
-			},
-			{
-				value : Math.random(),
-				color: "#7D4F6D"
-			},
-			{
-				value : Math.random(),
-				color: "#584A5E"
+	var pieData = [
+				{
+					value: 30,
+					color:"#F38630"
+				},
+				{
+					value : 50,
+					color : "#E0E4CC"
+				},
+				{
+					value : 100,
+					color : "#69D2E7"
+				}
+			
+			];
+
+	var myPie = new Chart(document.getElementById("canvas2").getContext("2d")).Pie(pieData);
+
+
+		$('#canvas2').hide();
+		$('#nextBtn').click(
+			function(){
+				$('#canvas').hide();
+				$('#canvas2').show();
 			}
-		];
-	var myPolarArea = new Chart(document.getElementById("canvas2").getContext("2d")).PolarArea(chartData);
+		);
+
+		$('#prevBtn').click(
+			function(){
+				$('#canvas2').hide();
+				$('#canvas').show();
+			}
+		);
+});
